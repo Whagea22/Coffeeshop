d94adcbe0b5a06f8fc1b32a27f8658e3b48c56f8
WHAGEA SAPUTRA <whageasaputra01@gmail.com>
2023-11-19 14:56:29 +0700

    update lanjutn

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ document.querySelector('#hamburger-menu').onclick = () => {
     navbarNav.classList.toggle('active');
 };
 
+// toggle class active // untk shopping cart
+const shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#shopping-cart-button').onclick = () => {
+    shoppingCart.classList.toggle('active');
+};
+
 // toggle class active untuk search form //
 const searchForm = document.querySelector('.search-form');
 const searchBox = document.querySelector('#search-box');
@@ -17,6 +23,7 @@ document.querySelector('#search-button').onclick = (e) => {
 
 // klik diluar element
 const hm = document.querySelector('#hamburger-menu');
+const sc = document.querySelector('#shopping-cart-button');
 const sb = document.querySelector('#search-button');
 
 
@@ -27,4 +34,32 @@ document.addEventListener('click', function(e) {
     if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
         searchForm.classList.remove('active');
     }
+    if(!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove('active');
+    }
 });
+
+
+// modal box //
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button');
+
+itemDetailButtons.forEach((btn) => {
+    btn.onclick = (e) => {
+        itemDetailModal.style.display = 'flex';
+        e.preventDefault();
+    };
+});
+
+// close modal 
+document.querySelector('.modal .close-icon').onclick = (e) => {
+    itemDetailModal.style.display = 'none';
+    e.preventDefault();
+}
+
+// close diluar modal
+window.onclick = (e) => {
+    if (e.target === itemDetailModal) {
+        itemDetailModal.style.display = 'none'
+    }
+};
